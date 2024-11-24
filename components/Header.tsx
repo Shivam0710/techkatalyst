11143b3091d99e53d5641ca05035f42e36c28541
Shivam0710 <39822449+Shivam0710@users.noreply.github.com>
2024-11-25 01:16:37 +0530

    Header tag fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Logo from '@/public/logo.png'
 import Image from 'next/image'
 import { inter } from '@/helpers/fonts'
+import Link from 'next/link'
 
 export const Header = () => {
   return (
     <div className='flex justify-between py-6 lg:py-10 px-6 lg:px-[100px] items-center'>
         <Image src={Logo} alt="Logo" className="w-[130px] lg:w-[216px] h-[40px] lg:h-[66px]" /> 
         <article className={`hidden lg:flex items-center space-x-20 ${inter.className} font-medium`}>
-            <a className='uppercase font-medium' href="/"> Home </a>
-            <a className='uppercase font-medium' href="/about-us"> About us </a>
-            <a className='uppercase font-medium' href="/services/mobile-app-development"> Services </a>
-            <a className='bg-[#7000FF] py-3 px-8 text-white rounded-sm font-medium' href="/contact-us"> Contact us </a> 
+            <Link className='uppercase font-medium' href="/"> Home </Link>
+            <Link className='uppercase font-medium' href="/about-us"> About us </Link>
+            <Link className='uppercase font-medium' href="/services/mobile-app-development"> Services </Link>
+            <Link className='bg-[#7000FF] py-3 px-8 text-white rounded-sm font-medium' href="/contact-us"> Contact us </Link>
         </article>
     </div>
   )
