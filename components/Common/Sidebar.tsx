d8663e5fc8c637abcbd09fc7285fd9bdd32ff26f
Shivam0710 <39822449+Shivam0710@users.noreply.github.com>
2024-11-30 22:29:37 +0530

    Design changes

--- components/Common/Sidebar.tsx
+++ components/Common/Sidebar.tsx
@@ -14,8 +14,8 @@ export const Sidebar = () => {
             <Drawer anchor='right' open={open} onClose={() => setOpen(false)}>
                 <div className='w-[240px] py-20 bg-[#9EE463] flex flex-col gap-6 justify-start text-center text-[#7000FF] relative h-screen'>
                     <CircleX color="#5A3AEC" className='absolute top-4 right-4 cursor-pointer' onClick={() => setOpen(false)} />
-                    <Link style={{ color: "#7000FF" }} className='uppercase font-medium text-[#7000FF] no-underline' href="/"> Home </Link>
-                    <Link style={{ color: "#7000FF" }} className='uppercase font-medium text-[#7000FF] no-underline' href="/about-us"> About us </Link>
+                    <Link style={{ color: "#7000FF", textDecoration: 'none', fontWeight: 'medium' }} className='uppercase font-medium text-[#7000FF] no-underline' href="/"> Home </Link>
+                    <Link style={{ color: "#7000FF", textDecoration: 'none', fontWeight: 'medium' }} className='uppercase font-medium text-[#7000FF] no-underline' href="/about-us"> About us </Link>
                     <div className=''>
                         <button 
                             className='uppercase font-medium flex items-center justify-self-center'
@@ -27,27 +27,27 @@ export const Sidebar = () => {
                             </svg>
                         </button>
                         <div className={`flex flex-col gap-2 overflow-hidden transition-all duration-300 ${!dropdown ? 'max-h-0' : 'max-h-full mt-2'}`}>
-                            <Link style={{ color: "#7000FF" }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/mobile-app-development">
+                            <Link style={{ color: "#7000FF", fontWeight: 'medium', textDecoration: 'none' }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/mobile-app-development">
                                 Mobile App Development
                             </Link>
-                            <Link style={{ color: "#7000FF" }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/web-development">
+                            <Link style={{ color: "#7000FF", fontWeight: 'medium', textDecoration: 'none' }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/web-development">
                                 Web Development
                             </Link>
-                            <Link style={{ color: "#7000FF" }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/ui-ux-design">
+                            <Link style={{ color: "#7000FF", fontWeight: 'medium', textDecoration: 'none' }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/ui-ux-design">
                                 UI/UX Design
                             </Link>
-                            <Link style={{ color: "#7000FF" }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/market-research">
+                            <Link style={{ color: "#7000FF", fontWeight: 'medium', textDecoration: 'none' }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/market-research">
                                 Market Research
                             </Link>
-                            <Link style={{ color: "#7000FF" }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/product-management">
+                            <Link style={{ color: "#7000FF", fontWeight: 'medium', textDecoration: 'none' }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/product-management">
                                 Product Management
                             </Link>
-                            <Link style={{ color: "#7000FF" }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/analytics-data-science">
+                            <Link style={{ color: "#7000FF", fontWeight: 'medium', textDecoration: 'none' }} className='text-[#7000FF] font-bold no-underline px-4 py-2 hover:bg-gray-100' href="/services/analytics-data-science">
                                 Analytics and Data science
                             </Link>
                         </div>
                     </div>
-                    <Link style={{ background: "#7000FF", color: "#fff", padding: "12px 24px" }} className='font-medium mx-3' href="/contact-us">
+                    <Link style={{ background: "#7000FF", color: "#fff", margin: "0 12px", padding: "12px 24px", fontWeight: "medium", textDecoration: 'none' }} href="/contact-us">
                         Contact us
                     </Link>
                 </div>
